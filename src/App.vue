6d54ad9144cda8cc2f6a5ee021842e54b05c40ff
DomenicoAgostino <99025293+DomenicoAgostino@users.noreply.github.com>
2022-05-12 16:31:52 +0200

    Bonus Completato - Jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <TitleComp />
+    <HeroJumbo />
   </div>
 </template>
 
 <script>
 import TitleComp from './components/TitleComp.vue'
+import HeroJumbo from './components/HeroJumbo.vue'
 
 export default {
   name: 'App',
   components: {
-    TitleComp
+    TitleComp,
+    HeroJumbo,
   }
 }
 </script>
---
+++ src/components/HeroJumbo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="jumbotron">
+    <h2>My App</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroJumbo'
+}
+</script>
+
+<style>
+.jumbotron{
+  background-color: red;
+  width: 100%;
+  height: 200px;
+}
+
+h2{
+  text-align: center;
+  font-size: 150px;
+  color: chartreuse;
+
+}
+</style>
